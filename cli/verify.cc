3095a1b3367e754c088903c6c56f203444d6b77f
Hannes Matuschek <hmatuschek@gmail.com>
2020-11-03 13:28:56 +0100

    Added code-plug verification to CLI.

--- cli/verify.cc
+++ cli/verify.cc
@@ -8,6 +8,11 @@
 #include "logger.hh"
 #include "config.hh"
 #include "csvreader.hh"
+#include "dfufile.hh"
+#include "rd5r.hh"
+#include "uv390.hh"
+#include "gd77.hh"
+#include "opengd77.hh"
 
 
 int verify(QCommandLineParser &parser, QCoreApplication &app)
@@ -24,20 +29,62 @@ int verify(QCommandLineParser &parser, QCoreApplication &app)
     return -1;
   }
 
+  Config config;
+
   // Determin type by ending or flag
   if (parser.isSet("csv") || (filename.endsWith(".conf") || filename.endsWith(".csv"))) {
     QTextStream stream(&file);
     QString errorMessage;
-    Config config;
     if (! CSVReader::read(&config, stream, errorMessage)) {
       logError() << "Cannot read config file '" << filename << "': " << errorMessage;
       return -1;
     }
-    logDebug() << "Verify '" << filename << "': No issues found.";
+    logInfo() << "Verify '" << filename << "': No syntax issues found.";
+  } else if (parser.isSet("bin") || (filename.endsWith(".bin") || filename.endsWith(".dfu"))) {
+    logError() << "Verification of binary code-plugs makes no sense.";
+    return -1;
+  } else {
+    logError() << "Cannot determine filetype from filename '" << filename
+               << "': Consider using --csv.";
+    return -1;
+  }
+
+  if (! parser.isSet("radio")) {
+    logInfo() << "To verify the codeplug against a specific radio, conser using the --radio=RADIO option.";
     return 0;
   }
 
-  logError() << "Cannot determine filetype from filename '" << filename
-             << "': Consider using --csv or --bin.";
-  return -1;
+  QList<VerifyIssue> issues;
+  QString radio = parser.value("radio").toLower();
+  if ("rd5r" == radio) {
+    RD5R radio; radio.verifyConfig(&config, issues);
+  } else if (("uv390" == radio) && ("rt3s" == radio)) {
+    UV390 radio; radio.verifyConfig(&config, issues);
+  } else if ("gd77" == radio) {
+    GD77 radio; radio.verifyConfig(&config, issues);
+  } else if ("opengd77" == radio) {
+    OpenGD77 radio; radio.verifyConfig(&config, issues);
+  } else {
+    logError() << "Cannot verify code-plug against unknown radio '" << radio << "'.";
+    return -1;
+  }
+
+  bool valid = true;
+  if (0 == issues.size()) {
+    logInfo() << "No issues found for code-plug '" << filename
+              << "' with radio '" << radio << "'.";
+  } else {
+    logInfo() << "Some issues found for code-plug '" << filename
+              << "' with radio '" << radio << "'.";
+    foreach (const VerifyIssue &issue, issues) {
+      if (VerifyIssue::WARNING == issue.type()) {
+        logWarn() << issue.message();
+      } else if (VerifyIssue::ERROR == issue.type()) {
+        logError() << issue.message();
+        valid = false;
+      }
+    }
+  }
+
+  return (valid ? 0 : -1);
 }
