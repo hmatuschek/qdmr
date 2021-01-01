0f16790b5bebaf01aa542b1d407d138d8d2ab1df
Hannes Matuschek <hmatuschek@gmail.com>
2021-01-01 15:11:27 +0100

    Fixed error messages in USB serial interface.

--- lib/usbserial.cc
+++ lib/usbserial.cc
@@ -61,10 +61,10 @@ USBSerial::errorMessage() const {
 
 void
 USBSerial::onError(QSerialPort::SerialPortError err) {
-  logError() <<": Serial port error: (" << err << ") " << errorString() << ".";
+  logError() <<"Serial port error: (" << err << ") " << errorString() << ".";
 }
 
 void
 USBSerial::onClose() {
-  logDebug() << ": Serial port will close now.";
+  logDebug() << "Serial port will close now.";
 }
