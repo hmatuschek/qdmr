d568dd7c7bf6a7635d7ba34d428e21e5cec9b0bf
Hannes Matuschek <hmatuschek@gmail.com>
2021-09-16 16:58:02 +0200

    Cleanup.

--- lib/dm1701.cc
+++ lib/dm1701.cc
@@ -3,8 +3,6 @@
 #include "logger.hh"
 #include "utils.hh"
 
-#define BSIZE 1024
-
 static Radio::Features _dm1701_features =
 {
   .betaWarning = false,
@@ -12,10 +10,11 @@ static Radio::Features _dm1701_features =
   .hasDigital = true,
   .hasAnalog = true,
 
-  .vhfLimits = {136., 174.},
-  .uhfLimits = {400., 480.},
+  .frequencyLimits = QVector<Radio::Features::FrequencyRange>{ {136., 174.}, {400., 480.} },
 
-  .maxNameLength = 16,
+  .maxRadioIDs        = 1,  /// @todo UV390 supports multiple radio IDs, not implemented yet.
+  .needsDefaultRadioID = true,
+
   .maxIntroLineLength = 10,
 
   .maxChannels = 3000,
@@ -57,8 +56,8 @@ static Radio::Features _dm1701_features =
 };
 
 
-DM1701::DM1701(QObject *parent)
-  : Radio(parent), _name("Baofeng DM-1701"), _dev(nullptr), _codeplugFlags(), _config(nullptr)
+DM1701::DM1701(DFUDevice *device, QObject *parent)
+  : TyTRadio(device, parent), _name("Baofeng DM-1701"), _codeplug(), _callsigns()
 {
   // pass...
 }
@@ -81,284 +80,4 @@ DM1701::codeplug() const {
 CodePlug &
 DM1701::codeplug() {
   return _codeplug;
-}
-
-bool
-DM1701::startDownload(bool blocking) {
-  if (StatusIdle != _task)
-    return false;
-
-  _task = StatusDownload;
-
-  if (blocking) {
-    run();
-    return (StatusIdle == _task);
-  }
-
-  start();
-  return true;
-}
-
-bool
-DM1701::startUpload(Config *config, bool blocking, const CodePlug::Flags &flags) {
-  if (StatusIdle != _task)
-    return false;
-
-  if (! (_config = config))
-    return false;
-
-  _task = StatusUpload;
-  _codeplugFlags = flags;
-  if (blocking) {
-    this->run();
-    return (StatusIdle == _task);
-  }
-
-  this->start();
-  return true;
-}
-
-bool
-DM1701::startUploadCallsignDB(UserDatabase *db, bool blocking, const CallsignDB::Selection &selection) {
-  if (StatusIdle != _task)
-    return false;
-
-  _callsigns.encode(db, selection);
-
-  _task = StatusUploadCallsigns;
-  if (blocking) {
-    this->run();
-    return (StatusIdle == _task);
-  }
-
-  this->start();
-  return true;
-}
-
-void
-DM1701::run() {
-  if (StatusDownload == _task) {
-    download();
-  } else if (StatusUpload == _task) {
-    upload();
-  } else if (StatusUploadCallsigns == _task) {
-    uploadCallsigns();
-  }
-}
-
-
-void
-DM1701::download() {
-  emit downloadStarted();
-  logDebug() << "Download of " << _codeplug.image(0).numElements() << " elements.";
-
-  _dev = new DFUDevice(0x0483, 0xdf11, this);
-  if (!_dev->isOpen()) {
-    _errorMessage = QString("Cannot open device at 0483:DF11: %1").arg(_dev->errorMessage());
-    _dev->deleteLater();
-    _task = StatusError;
-    emit downloadError(this);
-    return;
-  }
-
-  // Check every segment in the codeplug
-  size_t totb = 0;
-  for (int n=0; n<_codeplug.image(0).numElements(); n++) {
-    if (! _codeplug.image(0).element(n).isAligned(BSIZE)) {
-      _errorMessage = QString("%1 Cannot download codeplug: Codeplug element %2 (addr=%3, size=%4) "
-                              "is not aligned with blocksize %5.").arg(__func__)
-          .arg(n).arg(_codeplug.image(0).element(n).address())
-          .arg(_codeplug.image(0).element(n).data().size()).arg(BSIZE);
-      logError() << _errorMessage;
-      _task = StatusError;
-      _dev->reboot();
-      _dev->close();
-      _dev->deleteLater();
-      emit downloadError(this);
-      return;
-    }
-    totb += _codeplug.image(0).element(n).data().size()/BSIZE;
-  }
-
-  // Then download codeplug
-  size_t bcount = 0;
-  for (int n=0; n<_codeplug.image(0).numElements(); n++) {
-    uint addr = _codeplug.image(0).element(n).address();
-    uint size = _codeplug.image(0).element(n).data().size();
-    uint b0 = addr/BSIZE, nb = size/BSIZE;
-    for (uint b=0; b<nb; b++, bcount++) {
-      if (! _dev->read(0, (b0+b)*BSIZE, _codeplug.data((b0+b)*BSIZE), BSIZE)) {
-        _errorMessage = QString("%1 Cannot download codeplug: %2").arg(__func__)
-            .arg(_dev->errorMessage());
-        logError() << _errorMessage;
-        _task = StatusError;
-        _dev->reboot();
-        _dev->close();
-        _dev->deleteLater();
-        emit downloadError(this);
-        return;
-      }
-      emit downloadProgress(float(bcount*100)/totb);
-    }
-  }
-
-  _task = StatusIdle;
-  _dev->reboot();
-  _dev->close();
-  _dev->deleteLater();
-  emit downloadFinished(this, &_codeplug);
-  _config = nullptr;
-}
-
-void
-DM1701::upload() {
-  emit uploadStarted();
-
-  _dev = new DFUDevice(0x0483, 0xdf11, this);
-  if (!_dev->isOpen()) {
-    _errorMessage = QString("Cannot open device at 0483:DF11: %1").arg(_dev->errorMessage());
-    _dev->deleteLater();
-    _task = StatusError;
-    emit uploadError(this);
-    return;
-  }
-
-  // Check every segment in the codeplug
-  if (! _codeplug.isAligned(BSIZE)) {
-    _errorMessage = QString("%1 Cannot upload codeplug: "
-                            "Codeplug is not aligned with blocksize %5.").arg(__func__).arg(BSIZE);
-    logError() << _errorMessage;
-    _task = StatusError;
-    _dev->reboot();
-    _dev->close();
-    _dev->deleteLater();
-    emit downloadError(this);
-    return;
-  }
-
-  size_t totb = _codeplug.memSize();
-
-  size_t bcount = 0;
-  // If codeplug gets updated, download codeplug from device first:
-  if (_codeplugFlags.updateCodePlug) {
-    for (int n=0; n<_codeplug.image(0).numElements(); n++) {
-      uint addr = _codeplug.image(0).element(n).address();
-      uint size = _codeplug.image(0).element(n).data().size();
-      uint b0 = addr/BSIZE, nb = size/BSIZE;
-      for (uint b=0; b<nb; b++, bcount+=BSIZE) {
-        if (! _dev->read(0, (b0+b)*BSIZE, _codeplug.data((b0+b)*BSIZE), BSIZE)) {
-          _errorMessage = QString("%1 Cannot upload codeplug: %2").arg(__func__)
-              .arg(_dev->errorMessage());
-          logError() << _errorMessage;
-          _task = StatusError;
-          _dev->reboot();
-          _dev->close();
-          _dev->deleteLater();
-          emit downloadError(this);
-          return;
-        }
-        emit uploadProgress(float(bcount*50)/totb);
-      }
-    }
-  }
-
-  // Encode config into codeplug
-  logDebug() << "Encode call-sign DB.";
-  _codeplug.encode(_config, _codeplugFlags);
-
-  // then erase memory
-  for (int i=0; i<_codeplug.image(0).numElements(); i++)
-    _dev->erase(_codeplug.image(0).element(i).address(), _codeplug.image(0).element(i).memSize());
-
-  logDebug() << "Upload " << _codeplug.image(0).numElements() << " elements.";
-  // then, upload modified codeplug
-  bcount = 0;
-  for (int n=0; n<_codeplug.image(0).numElements(); n++) {
-    uint addr = _codeplug.image(0).element(n).address();
-    uint size = _codeplug.image(0).element(n).memSize();
-    uint b0 = addr/BSIZE, nb = size/BSIZE;
-    for (size_t b=0; b<nb; b++,bcount+=BSIZE) {
-      if (! _dev->write(0, (b0+b)*BSIZE, _codeplug.data((b0+b)*BSIZE), BSIZE)) {
-        _errorMessage = QString("%1 Cannot upload codeplug: %2").arg(__func__)
-            .arg(_dev->errorMessage());
-        logError() << _errorMessage;
-        _task = StatusError;
-        _dev->reboot();
-        _dev->close();
-        _dev->deleteLater();
-        emit uploadError(this);
-        return;
-      }
-      emit uploadProgress(50+float(bcount*50)/totb);
-    }
-  }
-
-  _task = StatusIdle;
-  _dev->reboot();
-  _dev->close();
-  _dev->deleteLater();
-
-  emit uploadComplete(this);
-}
-
-void
-DM1701::uploadCallsigns() {
-  emit uploadStarted();
-
-  _dev = new DFUDevice(0x0483, 0xdf11, this);
-  if (!_dev->isOpen()) {
-    _errorMessage = QString("Cannot open device at 0483:DF11: %1").arg(_dev->errorMessage());
-    _dev->deleteLater();
-    _task = StatusError;
-    emit uploadError(this);
-    return;
-  }
-
-  logDebug() << "Check alignment.";
-  // Check alignment in the codeplug
-  if (! _callsigns.isAligned(BSIZE)) {
-    _errorMessage = QString("%1 Cannot upload callsign db: Callsign DB is not aligned with blocksize %5.").arg(__func__);
-    logError() << _errorMessage;
-    _task = StatusError;
-    _dev->reboot();
-    _dev->close();
-    _dev->deleteLater();
-    emit downloadError(this);
-    return;
-  }
-
-  // then erase memory
-  logDebug() << "Erase memory section for call-sign DB.";
-  _dev->erase(_callsigns.image(0).element(0).address(),
-              _callsigns.image(0).element(0).memSize(),
-              [](uint percent, void *ctx) { emit ((DM1701 *)ctx)->uploadProgress(percent/2); }, this);
-
-  logDebug() << "Upload " << _callsigns.image(0).numElements() << " elements.";
-  // Total amount of data to transfer
-  size_t totb = _callsigns.memSize();
-  // Upload callsign DB
-  uint addr = _callsigns.image(0).element(0).address();
-  uint size = _callsigns.image(0).element(0).memSize();
-  uint b0 = addr/BSIZE, nb = size/BSIZE;
-  for (size_t b=0, bcount=0; b<nb; b++,bcount+=BSIZE) {
-    if (! _dev->write(0, (b0+b)*BSIZE, _callsigns.data((b0+b)*BSIZE), BSIZE)) {
-      _errorMessage = QString("%1 Cannot upload codeplug: %2").arg(__func__)
-          .arg(_dev->errorMessage());
-      logError() << _errorMessage;
-      _task = StatusError;
-      _dev->reboot();
-      _dev->close();
-      _dev->deleteLater();
-      emit uploadError(this);
-      return;
-    }
-    emit uploadProgress(50+float(bcount*50)/totb);
-  }
-
-  _task = StatusIdle;
-  _dev->reboot();
-  _dev->close();
-  _dev->deleteLater();
-
-  emit uploadComplete(this);
 }
--- lib/dm1701.hh
+++ lib/dm1701.hh
@@ -14,15 +14,18 @@
  *   - 250 RX group lists with ?? contacts each
  *   - 250 zones, 64 channels each (A & B)
  *   - 250 scan lists, 31 channels each
- *   -
+ *   - ...
  * @ingroup dsc */
 #ifndef DM1701_HH
 #define DM1701_HH
 
 #include "radio.hh"
-#include "dfu_libusb.hh"
+#include "tyt_radio.hh"
+#include "tyt_codeplug.hh"
+#include "tyt_callsigndb.hh"
 #include "uv390_codeplug.hh"
 #include "uv390_callsigndb.hh"
+
 
 /** Implements an USB interface to the  Baofeng DM-1701 and Retevis RT84 VHF/UHF 5W DMR (Tier I&II) radios.
  *
@@ -31,50 +34,22 @@
  * using DFU protocol.
  *
  * @ingroup dm1701 */
-class DM1701: public Radio
+class DM1701: public TyTRadio
 {
 	Q_OBJECT
 
 public:
   /** Do not construct this class directly, rather use @c Radio::detect. */
-  explicit DM1701(QObject *parent=nullptr);
+  explicit DM1701(DFUDevice *device=nullptr, QObject *parent=nullptr);
 
   const QString &name() const;
   const Radio::Features &features() const;
   const CodePlug &codeplug() const;
   CodePlug &codeplug();
 
-public slots:
-  /** Starts the download of the codeplug and derives the generic configuration from it. */
-  bool startDownload(bool blocking=false);
-  /** Derives the device-specific codeplug from the generic configuration and uploads that
-   * codeplug to the radio. */
-  bool startUpload(Config *config, bool blocking=false,
-                   const CodePlug::Flags &flags = CodePlug::Flags());
-  /** Encodes the given user-database and uploades it to the device. */
-  bool startUploadCallsignDB(UserDatabase *db, bool blocking=false,
-                             const CallsignDB::Selection &selection=CallsignDB::Selection());
-
-protected:
-  /** Thread main routine, performs all blocking IO operations for codeplug up- and download. */
-	void run();
-
-private:
-  void download();
-  void upload();
-  void uploadCallsigns();
-
 protected:
   /** The device identifier. */
 	QString _name;
-  /** The interface to the radio. */
-	DFUDevice *_dev;
-  /** Holds the flags to controll assembly and upload of code-plugs. */
-  CodePlug::Flags _codeplugFlags;
-  /** The generic configuration. */
-	Config *_config;
-  /** A weak reference to the user-database. */
-  UserDatabase *_userDB;
   /** The actual binary codeplug representation. */
 	UV390Codeplug _codeplug;
   /** The acutal binary callsign-db representation. */
