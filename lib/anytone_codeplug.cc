87b48347ed654d6ddd444a9e1ecd38ec2ad4b907
Hannes Matuschek <hmatuschek@gmail.com>
2021-09-17 10:39:58 +0200

    Started update of AnyTone codeplugs.

--- lib/anytone_codeplug.cc
+++ lib/anytone_codeplug.cc
@@ -1,5 +1,170 @@
 #include "anytone_codeplug.hh"
 
+/* ********************************************************************************************* *
+ * Implementation of AnytoneCodeplug::ChannelElement
+ * ********************************************************************************************* */
+AnytoneCodeplug::ChannelElement::ChannelElement(uint8_t *ptr, uint size)
+  : Element(ptr, size)
+{
+  // pass...
+}
+
+AnytoneCodeplug::ChannelElement::ChannelElement(uint8_t *ptr)
+  : Element(ptr, 0x0040)
+{
+  // pass...
+}
+
+AnytoneCodeplug::ChannelElement::~ChannelElement() {
+  // pass...
+}
+
+void
+AnytoneCodeplug::ChannelElement::clear() {
+  setRXFrequency(0);
+  setTXOffset(0);
+  setMode(Mode::Analog);
+  setPower(Channel::Power::Low);
+  setBandwidth(AnalogChannel::Bandwidth::Narrow);
+  setBit(0x0008, 5, false); // Unused set to 0
+}
+
+uint
+AnytoneCodeplug::ChannelElement::rxFrequency() const {
+  return ((uint)getBCD8_be(0x0000))*10;
+}
+void
+AnytoneCodeplug::ChannelElement::setRXFrequency(uint hz) {
+  setBCD8_be(0x0000, hz/10);
+}
+
+uint
+AnytoneCodeplug::ChannelElement::txOffset() const {
+  return ((uint)getBCD8_be(0x0004))*10;
+}
+void
+AnytoneCodeplug::ChannelElement::setTXOffset(uint hz) {
+  setBCD8_be(0x0004, hz/10);
+}
+
+AnytoneCodeplug::ChannelElement::Mode
+AnytoneCodeplug::ChannelElement::mode() const {
+  return (Mode) getUInt2(0x0008, 0);
+}
+void
+AnytoneCodeplug::ChannelElement::setMode(Mode mode) {
+  setUInt2(0x0008, 0, (uint)mode);
+}
+
+Channel::Power
+AnytoneCodeplug::ChannelElement::power() const {
+  switch ((Power)getUInt2(0x0008, 2)) {
+  case POWER_LOW: return Channel::Power::Low;
+  case POWER_MIDDLE: return Channel::Power::Mid;
+  case POWER_HIGH: return Channel::Power::High;
+  case POWER_TURBO: return Channel::Power::Max;
+  }
+  return Channel::Power::Low;
+}
+void
+AnytoneCodeplug::ChannelElement::setPower(Channel::Power power) {
+  switch (power) {
+  case Channel::Power::Min:
+  case Channel::Power::Low:
+    setUInt2(0x0008, 2, (uint)POWER_LOW);
+    break;
+  case Channel::Power::Mid:
+    setUInt2(0x0008, 2, (uint)POWER_MIDDLE);
+    break;
+  case Channel::Power::High:
+    setUInt2(0x0008, 2, (uint)POWER_HIGH);
+    break;
+  case Channel::Power::Max:
+    setUInt2(0x0008, 2, (uint)POWER_TURBO);
+    break;
+  }
+}
+
+AnalogChannel::Bandwidth
+AnytoneCodeplug::ChannelElement::bandwidth() const {
+  if (getBit(0x0008, 4))
+    return AnalogChannel::Bandwidth::Wide;
+  return AnalogChannel::Bandwidth::Narrow;
+}
+void
+AnytoneCodeplug::ChannelElement::setBandwidth(AnalogChannel::Bandwidth bw) {
+  switch (bw) {
+  case AnalogChannel::Bandwidth::Narrow: setBit(0x0008, 4, false); break;
+  case AnalogChannel::Bandwidth::Wide: setBit(0x0008, 4, true); break;
+  }
+}
+
+AnytoneCodeplug::ChannelElement::RepeaterMode
+AnytoneCodeplug::ChannelElement::repeaterMode() const {
+  return (RepeaterMode)getUInt2(0x0008, 6);
+}
+void
+AnytoneCodeplug::ChannelElement::setRepeaterMode(RepeaterMode mode) {
+  setUInt2(0x0008, 6, (uint)mode);
+}
+
+AnytoneCodeplug::ChannelElement::SignalingMode
+AnytoneCodeplug::ChannelElement::rxSignalingMode() const {
+  return (SignalingMode)getUInt2(0x0009, 0);
+}
+void
+AnytoneCodeplug::ChannelElement::setRXSignalingMode(SignalingMode mode) {
+  setUInt2(0x0009, 0, (uint)mode);
+}
+
+AnytoneCodeplug::ChannelElement::SignalingMode
+AnytoneCodeplug::ChannelElement::txSignalingMode() const {
+  return (SignalingMode)getUInt2(0x0009, 2);
+}
+void
+AnytoneCodeplug::ChannelElement::setTXSignalingMode(SignalingMode mode) {
+  setUInt2(0x0009, 2, (uint)mode);
+}
+
+bool
+AnytoneCodeplug::ChannelElement::ctcssPhaseReversal() const {
+  return getBit(0x0009, 4);
+}
+void
+AnytoneCodeplug::ChannelElement::enableCTCSSPhaseReversal(bool enable) {
+  setBit(0x0009, 4, enable);
+}
+bool
+AnytoneCodeplug::ChannelElement::rxOnly() const {
+  return getBit(0x0009, 5);
+}
+void
+AnytoneCodeplug::ChannelElement::enableRXOnly(bool enable) {
+  setBit(0x0009, 5, enable);
+}
+bool
+AnytoneCodeplug::ChannelElement::callConfirm() const {
+  return getBit(0x0009, 6);
+}
+void
+AnytoneCodeplug::ChannelElement::enableCallConfirm(bool enable) {
+  setBit(0x0009, 6, enable);
+}
+bool
+AnytoneCodeplug::ChannelElement::talkaround() const {
+  return getBit(0x0009, 7);
+}
+void
+AnytoneCodeplug::ChannelElement::enableTalkaround(bool enable) {
+  setBit(0x0009, 7, enable);
+}
+
+
+
+
+/* ********************************************************************************************* *
+ * Implementation of AnytoneCodeplug
+ * ********************************************************************************************* */
 AnytoneCodeplug::AnytoneCodeplug(QObject *parent)
   : CodePlug(parent)
 {
--- lib/anytone_codeplug.hh
+++ lib/anytone_codeplug.hh
@@ -14,6 +14,121 @@ class AnytoneCodeplug : public CodePlug
 {
   Q_OBJECT
 
+public:
+  /** Represents the base class for channel encodings in all AnyTone codeplugs.
+   *
+   * Memmory layout of encoded channel (64byte):
+   * @verbinclude anytone_channel.txt
+   */
+  class ChannelElement: public Element
+  {
+  public:
+    /** Defines all possible channel modes, see @c channelMode. */
+    enum class Mode {
+      Analog      = 0,               ///< Analog channel.
+      Digital     = 1,               ///< Digital (DMR) channel.
+      MixedAnalog = 2,               ///< Mixed, analog channel with digital RX.
+      MixedDigital = 3               ///< Mixed, digital channel with analog RX.
+    };
+
+    /** Defines all possible power settings.*/
+    enum Power {
+      POWER_LOW    = 0,              ///< Low power, usually 1W.
+      POWER_MIDDLE = 1,              ///< Medium power, usually 2.5W.
+      POWER_HIGH   = 2,              ///< High power, usually 5W.
+      POWER_TURBO  = 3               ///< Higher power, usually 7W on VHF and 6W on UHF.
+    };
+
+    /** Defines all possible repeater modes. */
+    enum class RepeaterMode {
+      Simplex  = 0,                  ///< Simplex mode, that is TX frequency = RX frequency. @c tx_offset is ignored.
+      Positive = 1,                  ///< Repeater mode with positive @c tx_offset.
+      Negative = 2                   ///< Repeater mode with negative @c tx_offset.
+    };
+
+    /** Possible analog signaling modes. */
+    enum class SignalingMode {
+      None = 0,                      ///< None.
+      CTCSS = 1,                     ///< Use CTCSS tones
+      DCS = 2                        ///< Use DCS codes.
+    };
+
+  protected:
+    /** Hidden constructor. */
+    ChannelElement(uint8_t *ptr, uint size);
+
+  public:
+    /** Constructor. */
+    ChannelElement(uint8_t *ptr);
+    /** Destructor. */
+    virtual ~ChannelElement();
+
+    /** Resets the channel. */
+    void clear();
+
+    /** Returns the RX frequency in Hz. */
+    virtual uint rxFrequency() const;
+    /** Sets the RX frequency in Hz. */
+    virtual void setRXFrequency(uint hz);
+
+    /** Returns the TX frequency offset in Hz.
+     * This method returns an unsigned value, the sign of the offset frequency is stored in
+     * @c repeaterMode(). */
+    virtual uint txOffset() const;
+    /** Sets the TX frequency offset in Hz.
+     * This method accepts unsigned values, the sign of the offset frequency is stored in
+     * @c repeaterMode(). */
+    virtual void setTXOffset(uint hz);
+
+    /** Returns the channel mode (analog, digtital, etc). */
+    virtual Mode mode() const;
+    /** Sets the channel mode. */
+    virtual void setMode(Mode mode);
+
+    /** Returns the channel power. */
+    virtual Channel::Power power() const;
+    /** Sets the channel power. */
+    virtual void setPower(Channel::Power power);
+
+    /** Returns the band width of the channel. */
+    virtual AnalogChannel::Bandwidth bandwidth() const;
+    /** Sets the band width of the channel. */
+    virtual void setBandwidth(AnalogChannel::Bandwidth bw);
+
+    /** Retuns the transmit offset direction. */
+    virtual RepeaterMode repeaterMode() const;
+    /** Sets the transmit offset direction. */
+    virtual void setRepeaterMode(RepeaterMode mode);
+
+    /** Returns the RX signaling mode */
+    virtual SignalingMode rxSignalingMode() const;
+    /** Sets the RX signaling mode */
+    virtual void setRXSignalingMode(SignalingMode mode);
+    /** Returns the TX signaling mode */
+    virtual SignalingMode txSignalingMode() const;
+    /** Sets the TX signaling mode */
+    virtual void setTXSignalingMode(SignalingMode mode);
+
+    /** Returns @c true if the CTCSS phase reversal is enabled. */
+    virtual bool ctcssPhaseReversal() const;
+    /** Enables/disables CTCSS phase reversal. */
+    virtual void enableCTCSSPhaseReversal(bool enable);
+    /** Returns @c true if the RX only is enabled. */
+    virtual bool rxOnly() const;
+    /** Enables/disables RX only. */
+    virtual void enableRXOnly(bool enable);
+    /** Returns @c true if the call confirm is enabled. */
+    virtual bool callConfirm() const;
+    /** Enables/disables call confirm. */
+    virtual void enableCallConfirm(bool enable);
+    /** Returns @c true if the talkaround is enabled. */
+    virtual bool talkaround() const;
+    /** Enables/disables talkaround. */
+    virtual void enableTalkaround(bool enable);
+
+
+  };
+
 protected:
   /** Hidden constructor. */
   explicit AnytoneCodeplug(QObject *parent=nullptr);
