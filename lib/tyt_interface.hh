aefb77772c13009b63380fba777acdc0f09688c9
Hannes Matuschek <hmatuschek@gmail.com>
2021-10-07 13:38:19 +0200

    Documented TyT interface.

--- lib/tyt_interface.hh
+++ lib/tyt_interface.hh
@@ -5,8 +5,86 @@
 #include "radiointerface.hh"
 
 /** Interface to TyT (and Retevis) devices.
- * This class extends the @c DFUDevice and implements the @c RadioInterface to identify the radio
- * and the reading/writing and erasing of memory.
+ *
+ * This class extends the @c DFUDevice and implements the @c RadioInterface to identify the radio,
+ * reading/writing and erasing codeplugs or the callsign DB.
+ *
+ * @section tytif TyT protocol
+ * The communication with the device is kind of weird. It all happens through memory reads and
+ * writes. This is not unusual as such, but they implemented a weird memory mapping. Everyhing
+ * written to block 0 (i.e., memory address 0x00000000) is a command to the radio. Responses to
+ * these commands are read back from the same address/block.
+ *
+ * The funny thing is, however, that there is a command that sets the memory read and write address,
+ * although the DFU interface actually provides means to specify that address. Moreover, commands
+ * and data are send and written to the same DFU interface number. Someone did not understood the
+ * DFU interface.
+ *
+ * @subsection Commands
+ * The following commands has been seen.
+ *
+ * @subsubsection tytifepm Enter program mode
+ * The request written is
+ * @code
+ * +------+------+
+ * | 0x91 | 0x01 |
+ * +------+------+
+ * @endcode
+ * There is no explict response read back.
+ *
+ * @subsubsection tytiferb Reboot
+ * The request written is
+ * @code
+ * +------+------+
+ * | 0x91 | 0x05 |
+ * +------+------+
+ * @endcode
+ * There is no explict response read back.
+ *
+ * @subsubsection tyifid Identify/Get Info
+ * The request written is
+ * @code
+ * +------+------+
+ * | 0xa2 | What |
+ * +------+------+
+ * @endcode
+ * There is always some response read back. The size varies whith whatever is read back.
+ * <table>
+ *  <tr><th>WhatCode</th> <th>Resp. Len.</th> <th>Description></th></tr>
+ *  <tr><td>0x01</td>     <td>32</td>         <td>The radio identifier as a string + some unknown
+ *          information.</td></tr>
+ *  <tr><td>0x02</td>     <td>4</td>          <td>Unknown</td></tr>
+ *  <tr><td>0x03</td>     <td>24</td>         <td>Unknown</td></tr>
+ *  <tr><td>0x04</td>     <td>8</td>          <td>Unknown</td></tr>
+ *  <tr><td>0x07</td>     <td>16</td>         <td>Unknown</td></tr>
+ * </table>
+ *
+ * @subsubsection tytidsa Set address
+ * The request written is
+ * @code
+ * +------+------+------+------+------+
+ * | 0x21 | Address little endian     |
+ * +------+------+------+------+------+
+ * @endcode
+ * There is no response read back. Before reading or writing memory, the address must be set using
+ * this command. The start block for reading/writing this memory, is 2.
+ *
+ * @subsubsection tytife Erase
+ * The request written is
+ * @code
+ * +------+------+------+------+------+
+ * | 0x41 | Address little endian     |
+ * +------+------+------+------+------+
+ * @endcode
+ * There is no response read. The erased page size appears to be 0x010000 bytes.
+ *
+ * @subsection tytifrw Read and write memory
+ * Before writing memory, it must be erased.
+ *
+ * Before reading and writing memory, the address must be set.
+ *
+ * After this, the memory can be read/written by simply writing to consecutive blocks starting
+ * with block 2.
  *
  * @ingroup tyt */
 class TyTInterface : public DFUDevice, public RadioInterface
