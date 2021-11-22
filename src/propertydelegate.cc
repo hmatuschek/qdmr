e3ad006d5e225c8c6ad99f0ce663c0d990a41a9f
Hannes Matuschek <hmatuschek@gmail.com>
2021-11-22 14:30:03 +0100

    Fixed property delegate when editing boolean properties.

--- src/propertydelegate.cc
+++ src/propertydelegate.cc
@@ -30,24 +30,23 @@ PropertyDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &opti
   // Dispatch by type
   if (prop.isEnumType()) {
     return new QComboBox(parent);
-  } else if (QString("bool") == prop.typeName()) {
-    // should be handled by flags
-    return QItemDelegate::createEditor(parent, option, index);
-  } else if (QString("int") == prop.typeName()) {
+  } else if (QVariant::Bool == prop.type()) {
+    return new QComboBox(parent);
+  } else if (QVariant::Int == prop.type()) {
     QSpinBox *edit = new QSpinBox(parent);
     edit->setMinimum(std::numeric_limits<int>::min());
     edit->setMaximum(std::numeric_limits<int>::max());
     return edit;
-  } else if (QString("uint") == prop.typeName()) {
+  } else if (QVariant::UInt == prop.type()) {
     QSpinBox *edit = new QSpinBox(parent);
     edit->setMinimum(0);
     edit->setMaximum(std::numeric_limits<int>::max());
     return edit;
-  } else if (QString("double") == prop.typeName()) {
+  } else if (QVariant::Double == prop.type()) {
     QLineEdit *edit = new QLineEdit(parent);
     edit->setValidator(new QDoubleValidator(edit));
     return edit;
-  } else if (QString("QString") == prop.typeName()) {
+  } else if (QVariant::String == prop.type()) {
     return new QLineEdit(parent);
   } else if (prop.read(this).value<ConfigObjectReference *>()) {
     ConfigObjectReference *ref = prop.read(this).value<ConfigObjectReference *>();
@@ -74,23 +73,29 @@ PropertyDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
         if (etype.value(i) == prop.read(obj).toInt())
           box->setCurrentIndex(i);
     }
-  } else if (QString("bool") == prop.typeName()) {
-    // should be handled by flags
-    return QItemDelegate::setEditorData(editor, index);
-  } else if ((QString("int") == prop.typeName()) || (QString("uint") == prop.typeName())) {
+  } else if (QVariant::Bool == prop.type()) {
+    QComboBox *box = dynamic_cast<QComboBox *>(editor);
+    box->addItem(tr("False"), false);
+    box->addItem(tr("True"), true);
+    if (prop.read(obj).toBool())
+      box->setCurrentIndex(1);
+    else
+      box->setCurrentIndex(0);
+  } else if ((QVariant::Int == prop.type()) || (QVariant::UInt == prop.type())) {
     dynamic_cast<QSpinBox *>(editor)->setValue(prop.read(obj).toInt());
-  } else if (QString("double") == prop.typeName()) {
+  } else if (QVariant::Double == prop.type()) {
     dynamic_cast<QLineEdit *>(editor)->setText(QString::number(prop.read(obj).toDouble()));
-  } else if (QString("QString") == prop.typeName()) {
+  } else if (QVariant::String == prop.type()) {
     dynamic_cast<QLineEdit *>(editor)->setText(prop.read(obj).toString());
   } else if (prop.read(this).value<ConfigObjectReference *>()) {
     QComboBox *box = dynamic_cast<QComboBox*>(editor);
-    box->addItem(tr("[None]"));
+    box->addItem(tr("[None]"), QVariant::fromValue<ConfigObject*>(nullptr));
   }
 }
 
 void
 PropertyDelegate::setModelData(QWidget *editor, QAbstractItemModel *abstractmodel, const QModelIndex &index) const {
+  Q_UNUSED(abstractmodel);
   const PropertyWrapper *model = getModel(index.model());
   ConfigItem *obj = model->parentObject(index);
   QMetaProperty prop = model->propertyAt(index);
@@ -99,14 +104,13 @@ PropertyDelegate::setModelData(QWidget *editor, QAbstractItemModel *abstractmode
   // Dispatch by type
   if (prop.isEnumType()) {
     prop.write(obj, dynamic_cast<QComboBox *>(editor)->currentData());
-  } else if (QString("bool") == prop.typeName()) {
-    // should be handled by flags
-    return QItemDelegate::setModelData(editor, abstractmodel, index);
-  } else if ((QString("int") == prop.typeName()) || (QString("uint") == prop.typeName())) {
+  } else if (QVariant::Bool == prop.type()) {
+    prop.write(obj, dynamic_cast<QComboBox *>(editor)->currentData());
+  } else if ((QVariant::Int == prop.type()) || (QVariant::UInt == prop.type())) {
     prop.write(obj, dynamic_cast<QSpinBox *>(editor)->value());
-  } else if (QString("double") == prop.typeName()) {
+  } else if (QVariant::Double == prop.type()) {
     prop.write(obj, dynamic_cast<QLineEdit *>(editor)->text().toDouble());
-  } else if (QString("QString") == prop.typeName()) {
+  } else if (QVariant::String == prop.type()) {
     prop.write(obj, dynamic_cast<QLineEdit *>(editor)->text());
   } else if (prop.read(this).value<ConfigObjectReference *>()) {
     QComboBox *box = dynamic_cast<QComboBox*>(editor);
