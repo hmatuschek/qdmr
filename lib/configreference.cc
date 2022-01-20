dd517d02174f43af4e0bcdd4039ce2e8e0251865
Hannes Matuschek <hmatuschek@gmail.com>
2022-01-20 09:16:48 +0100

    Fixed stupid cast mistake in AbstractConfigObjectList. Addresses #194.

--- lib/configreference.cc
+++ lib/configreference.cc
@@ -81,7 +81,10 @@ ConfigObjectReference::allow(const QMetaObject *elementType) {
 
 void
 ConfigObjectReference::onReferenceDeleted(QObject *obj) {
-  Q_UNUSED(obj)
+  // Check if destroyed obj is referenced one.
+  if (_object != reinterpret_cast<ConfigObject*>(obj))
+    return;
+  // If it is
   _object = nullptr;
   emit modified();
 }
