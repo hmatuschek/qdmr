3caab628cf2ef60d0f2e66dca0a91a0785d6b358
Hannes Matuschek <hmatuschek@gmail.com>
2022-02-22 18:36:12 +0100

    Fixed MD2017 zone decoding (off-by-one error).

--- lib/md2017_codeplug.cc
+++ lib/md2017_codeplug.cc
@@ -236,7 +236,7 @@ MD2017Codeplug::linkZones(Context &ctx, const ErrorStack &err) {
       return false;
     }
     ZoneExtElement zoneext(data(ADDR_ZONEEXTS + i*ZONEEXT_SIZE));
-    if (! zoneext.linkZoneObj(ctx.get<Zone>(i), ctx)) {
+    if (! zoneext.linkZoneObj(ctx.get<Zone>(i+1), ctx)) {
       errMsg(err) << "Cannot link zone extension at index " << i << ".";
       return false;
     }
