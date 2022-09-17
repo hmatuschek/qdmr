e63c9fc554db6487a5f3be10d1de1d4a0ae4125d
Hannes Matuschek <hmatuschek@gmail.com>
2022-09-17 17:48:08 +0200

    Fixed manual. Addresses #251.

--- lib/codeplug.cc
+++ lib/codeplug.cc
@@ -587,7 +587,7 @@ Codeplug::Context::Context(Config *config)
 {
   // Add tables for common elements
   addTable(&DMRRadioID::staticMetaObject);
-  addTable(&DMRContact::staticMetaObject);
+  addTable(&DigitalContact::staticMetaObject);
   addTable(&DTMFContact::staticMetaObject);
   addTable(&RXGroupList::staticMetaObject);
   addTable(&Channel::staticMetaObject);
