03c785b3f36df3c7b57a09187c080713eb6ccf98
Hannes Matuschek <hmatuschek@gmail.com>
2024-04-26 11:31:16 +0200

    Implemented merge visitor.

--- lib/configmergevisitor.hh
+++ lib/configmergevisitor.hh
@@ -3,6 +3,18 @@
 
 #include "visitor.hh"
 
+class ConfigObject;
+class RadioID;
+class Channel;
+class Contact;
+class PositioningSystem;
+class RoamingChannel;
+class ConfigObjectRefList;
+class RXGroupList;
+class Zone;
+class ScanList;
+class RoamingZone;
+
 /** Implements a visitor to merge the visited config into a given config.
  * The destination configuration object is passed to the constructor. This allows to merge several
  * configurations into one. */
@@ -32,15 +44,35 @@ public:
    *        merged into.
    * @param itemStrategy Specifies the item merge strategy.
    * @param setStrategy Specifies the set merge strategy. */
-  ConfigMergeVisitor(Config *destination, ItemStrategy itemStrategy=ItemStrategy::Ignore,
+  ConfigMergeVisitor(Config *destination, QHash<ConfigItem *, ConfigItem *> &translation,
+                     ItemStrategy itemStrategy=ItemStrategy::Ignore,
                      SetStrategy setStrategy=SetStrategy::Ignore);
 
 
   bool processItem(ConfigItem *item, const ErrorStack &err=ErrorStack());
 
 protected:
+  bool processRadioID(RadioID *item, const ErrorStack &err = ErrorStack());
+  bool processChannel(Channel *item, const ErrorStack &err = ErrorStack());
+  bool processContact(Contact *item, const ErrorStack &err = ErrorStack());
+  bool processPositioningSystem(PositioningSystem *item, const ErrorStack &err = ErrorStack());
+  bool processRoamingChannel(RoamingChannel *item, const ErrorStack &err = ErrorStack());
+  bool processGroupList(RXGroupList *item, const ErrorStack &err = ErrorStack());
+  bool processZone(Zone *item, const ErrorStack &err = ErrorStack());
+  bool processScanList(ScanList *item, const ErrorStack &err = ErrorStack());
+  bool processRoamingZone(RoamingZone *item, const ErrorStack &err = ErrorStack());
+
+  bool addObject(AbstractConfigObjectList *list, ConfigObject *present, ConfigObject *merging, const ErrorStack &err = ErrorStack());
+  bool ignoreObject(AbstractConfigObjectList *list, ConfigObject *present, ConfigObject *merging, const ErrorStack &err = ErrorStack());
+  bool replaceObject(AbstractConfigObjectList *list, ConfigObject *present, ConfigObject *merging, const ErrorStack &err = ErrorStack());
+  bool duplicateObject(AbstractConfigObjectList *list, ConfigObject *present, ConfigObject *merging, const ErrorStack &err = ErrorStack());
+  bool mergeList(ConfigObjectRefList *present, ConfigObjectRefList *merging, const ErrorStack &err = ErrorStack());
+
+protected:
   /** The destination configuration. */
   Config *_destination;
+  /** Translation table for fixing references. */
+  QHash<ConfigItem *, ConfigItem *> &_translation;
   /** The item merge strategy. */
   ItemStrategy _itemStrategy;
   /** The set merge strategy. */
