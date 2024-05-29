b8cbe8ddbaba06bc21812eac627916c39cde6576
Hannes Matuschek <hannes.matuschek@uni-potsdam.de>
2024-05-29 09:00:26 +0200

    Added unit tests for CTCSS/DCS encoding/decoding. Addresses #414.

---
+++ test/gd73_test.hh
@@ -0,0 +1,22 @@
+#ifndef GD73TEST_HH
+#define GD73TEST_HH
+
+
+#include "libdmrconfigtest.hh"
+
+class GD73Test : public UnitTestBase
+{
+  Q_OBJECT
+
+public:
+  explicit GD73Test(QObject *parent = nullptr);
+
+private slots:
+  void testBasicConfigEncoding();
+  void testBasicConfigDecoding();
+  void testChannelFrequency();
+
+  void testFMSignaling();
+};
+
+#endif // GD73TEST_HH
