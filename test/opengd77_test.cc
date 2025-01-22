7da68b720dd52c55a5fa818fe2f2a784ccaf3dce
Hannes Matuschek <hmatuschek@gmail.com>
2025-01-22 21:33:33 +0100

    Fixed en/decoding of subtones in OpenGD77 firmwares. Also added regression test. Addresses #549. (#552)

--- test/opengd77_test.cc
+++ test/opengd77_test.cc
@@ -138,5 +138,33 @@ OpenGD77Test::testOverrideChannelRadioId() {
 }
 
 
+void
+OpenGD77Test::testChannelSubTones() {
+  ErrorStack err;
+  Config config, decoded;
+
+  if (! config.readYAML(":/data/fm_aprs_test.yaml", err)) {
+    QFAIL(QString("Cannot open codeplug file: %1")
+          .arg(err.format()).toLocal8Bit().constData());
+  }
+
+  config.channelList()->channel(0)->as<FMChannel>()->setTXTone(SelectiveCall(67.0));
+  config.channelList()->channel(0)->as<FMChannel>()->setRXTone(SelectiveCall(71.9));
+
+  if (! encodeDecode(config, decoded, err))
+    QFAIL(err.format().toLocal8Bit().constData());
+
+  SelectiveCall txTone = decoded.channelList()->channel(0)->as<FMChannel>()->txTone(),
+      rxTone = decoded.channelList()->channel(0)->as<FMChannel>()->rxTone();
+
+  QVERIFY(txTone.isValid());
+  QVERIFY(txTone.isCTCSS());
+  QCOMPARE(txTone.Hz(), 67.0);
+
+  QVERIFY(rxTone.isValid());
+  QVERIFY(rxTone.isCTCSS());
+  QCOMPARE(rxTone.Hz(), 71.9);
+}
+
 QTEST_GUILESS_MAIN(OpenGD77Test)
 
