24a6f2f8185a6d35a71c356042a2fa6bea496752
Hannes Matuschek <hannes.matuschek@uni-potsdam.de>
2026-01-19 10:31:40 +0100

    Fixed modality of about dialog. (#781)

--- src/application.cc
+++ src/application.cc
@@ -829,15 +829,21 @@ Application::showSettings() {
 
 void
 Application::showAbout() {
+
   QUiLoader loader;
   QFile uiFile("://ui/aboutdialog.ui");
   uiFile.open(QIODevice::ReadOnly);
-  QDialog *dialog = qobject_cast<QDialog *>(loader.load(&uiFile));
+  auto obj = loader.load(&uiFile);
+  if (nullptr == obj) return;
+  QDialog *dialog = qobject_cast<QDialog *>(obj);
+  if (nullptr == dialog) return;
+
   QTextEdit *text = dialog->findChild<QTextEdit *>("textEdit");
   text->setHtml(text->toHtml().arg(VERSION_STRING));
 
   QTreeWidget *radioTab = dialog->findChild<QTreeWidget *>("radioTable");
   radioTab->setColumnCount(1);
+
   QHash<QString, QTreeWidgetItem*> items;
   foreach (RadioInfo radio, RadioInfo::allRadios(false)) {
     if (! items.contains(radio.manufacturer()))
@@ -857,10 +863,8 @@ Application::showAbout() {
   radioTab->insertTopLevelItems(0, items.values());
   radioTab->sortByColumn(0,Qt::AscendingOrder);
 
-  if (dialog) {
-    dialog->exec();
-    dialog->deleteLater();
-  }
+  dialog->exec();
+  dialog->deleteLater();
 }
 
 void
