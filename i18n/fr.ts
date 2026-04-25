c782e3ed3de62cf81aeb05fd6180afcc67ad9019
Hannes Matuschek <dm3mat@noreply.codeberg.org>
2026-04-25 09:55:29 +0000

    Translated using Weblate (French)
    Currently translated at 71.0% (534 of 752 strings)
    
    Translation: qdmr/Graphical User Interface
    Translate-URL: https://translate.codeberg.org/projects/qdmr/graphical-user-interface/fr/

--- i18n/fr.ts
+++ i18n/fr.ts
@@ -238,7 +238,7 @@
     <message>
         <location filename="../src/application.cc" line="501"/>
         <source>The codeplug was successfully verified with the radio &apos;%1&apos;</source>
-        <translation type="unfinished">Le codeplug a été validé avec succès pour la radio &apos;%1&apos;</translation>
+        <translation>Le codeplug a été validé avec succès pour la radio &apos;%1&apos;.</translation>
     </message>
     <message>
         <location filename="../src/application.cc" line="545"/>
