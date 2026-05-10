82e01dc541b02a099cc36edce426fa31dd4f1157
nomore1007 <31173535+nomore1007@users.noreply.github.com>
2026-05-10 13:17:42 -0600

    Enable support for DMR6X2UV2 radio model (#919)

--- lib/radio.cc
+++ lib/radio.cc
@@ -78,6 +78,8 @@ Radio::detect(const USBDeviceDescriptor &descr, const RadioInfo &force, const Er
         return new D578UV(anytone);
       } else if ((id.isValid() && (RadioInfo::DMR6X2UV == id.id())) || (force.isValid() && (RadioInfo::DMR6X2UV == force.id()))) {
         return new DMR6X2UV(anytone);
+      } else if ((id.isValid() && (RadioInfo::DMR6X2UV2 == id.id())) || (force.isValid() && (RadioInfo::DMR6X2UV2 == force.id()))) {
+        return new DMR6X2UV(anytone);
       } else if ((id.isValid() && (RadioInfo::D578UV == id.id())) || (force.isValid() && (RadioInfo::D578UV == force.id()))) {
         return new D578UV(anytone);
       } else if (id.isValid()) {
