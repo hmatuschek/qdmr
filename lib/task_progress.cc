1dc6aa2ba9eb08eb9f12ae1e118b43a825873925
Hannes Matuschek <hannes.matuschek@uni-potsdam.de>
2026-05-20 15:43:45 +0200

    Added task progress class.

---
+++ lib/task_progress.cc
@@ -0,0 +1,56 @@
+#include "task_progress.hh"
+
+#include <QNetworkReply>
+
+
+/* ********************************************************************************************** *
+ * TaskProgress
+ * ********************************************************************************************** */
+TaskProgress::TaskProgress(const QString &label, QObject *parent)
+  : QObject{parent}, _label{label}, _selfDestructTimer() {
+  _selfDestructTimer.setSingleShot(true);
+  _selfDestructTimer.setInterval(10000);
+  connect(&_selfDestructTimer, &QTimer::timeout,
+    this, &TaskProgress::deleteLater);
+}
+
+const QString &
+TaskProgress::label() const {
+  return _label;
+}
+
+void
+TaskProgress::setProgress(int prog) {
+  prog = std::min(std::max(prog, 0), 100);
+  emit progress(prog);
+}
+
+void
+TaskProgress::finish(bool succ, const QString &message) {
+  if (succ)
+    emit success(message);
+  else
+    emit failed(message);
+  _selfDestructTimer.start();
+}
+
+
+
+/* ********************************************************************************************** *
+ * DownloadTaskProgress
+ * ********************************************************************************************** */
+DownloadTaskProgress::DownloadTaskProgress(const QString &label, QNetworkReply *reply, QObject *parent)
+  : TaskProgress{label, parent} {
+  connect(reply, &QNetworkReply::downloadProgress, [this](qint64 loaded, qint64 total) {
+    if (total > 0)
+      this->progress((100*loaded)/total);
+    else
+      this->progress(0);
+  });
+  connect(reply, &QNetworkReply::finished, [this, reply]() {
+    if (reply->error() != QNetworkReply::NoError)
+      this->finish(false, tr("Download failed."));
+    else
+      this->finish(true, tr("Download complete"));W
+  });
+}
---
+++ lib/task_progress.hh
@@ -0,0 +1,53 @@
+#ifndef QDMR_TASK_PROGRESS_HH
+#define QDMR_TASK_PROGRESS_HH
+
+#include <QObject>
+#include <QTimer>
+
+class QNetworkReply;
+
+
+/** Represents the current progress of a task. */
+class TaskProgress: public QObject
+{
+  Q_OBJECT
+
+public:
+  /** Constructs the progress for a task with the given label. */
+  TaskProgress(const QString &label, QObject *parent = nullptr);
+
+  /** Returns the label. */
+  const QString &label() const;
+
+signals:
+  /** Emits the current progress in percent. */
+  void progress(int progress);
+  /** Gets emitted if the task is finished successfully. */
+  void success(const QString &message);
+  /** Gets emitted if the task failed. */
+  void failed(const QString &message);
+
+public slots:
+  /** Sets the progress in percent. */
+  void setProgress(int progress);
+  /** Finishes the task with the given state and message. */
+  void finish(bool success, const QString &message="");
+
+protected:
+  /** The label of the task. */
+  QString _label;
+  /** Self-destruction timer. */
+  QTimer _selfDestructTimer;
+};
+
+
+/** A task progress for download tasks. */
+class DownloadTaskProgress: public TaskProgress
+{
+  Q_OBJECT
+
+public:
+  DownloadTaskProgress(const QString &label, QNetworkReply *reply, QObject *parent = nullptr);
+};
+
+#endif //QDMR_TASK_PROGRESS_HH
